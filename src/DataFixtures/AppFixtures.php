86cc8615ff7775aedcce80b4e29ca82995019218
Dolingale <contactdwwm@gmail.com>
2020-02-26 11:28:12 +0100

    Mercredi Midi

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -3,6 +3,8 @@
 namespace App\DataFixtures;
 
 use App\Entity\Ad;
+use Faker\Factory;
+use App\Entity\Image;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Common\Persistence\ObjectManager;
 
@@ -10,16 +12,34 @@ class AppFixtures extends Fixture
 {
     public function load(ObjectManager $manager)
     {
+
+        $faker = Factory::create('FR-fr');
+
         for($i = 1; $i <= 30; $i++){
             $ad = new Ad();
 
-            $ad->setTitle("Titre de l'annonce n°$i")
-            ->setSlug("Titre-de-l-annonce")
-            ->setCoverImage("http://placehold.it/100x300")
-            ->setIntroduction("Bonjour à tous c'est une introduction")
-            ->setContent("<p>Je suis un contenu riche !</p>")
+            $title = $faker->sentence();
+            $coverImage = $faker->imageUrl(1000,350); //->MARCHE PLUS CAR PLUS DE CERTIFICAT SSL
+            $introduction = $faker->paragraph(2);
+            $content = '<p>' . join('</p><p>',$faker->paragraphs(5)) .'</p>' ;
+
+            $ad->setTitle($title)
+            ->setCoverImage($coverImage)
+            ->setIntroduction($introduction)
+            ->setContent($content)
             ->setPrice(mt_rand(40, 200))
             ->setRooms(mt_rand(1, 5));
+
+            for($j = 1; $j <= mt_rand(2,5); $j++){
+                $image = new Image();
+
+                $image->setUrl($faker->imageUrl())
+                      ->setCaption($faker->sentence())
+                      ->setAd($ad);
+
+                $manager->persist($image);
+
+            }
         
            $manager->persist($ad);
         }
