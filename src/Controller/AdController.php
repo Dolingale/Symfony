a98c368b03e85b3e2f1af58dd88b200a47764965
Dolingale <contactdwwm@gmail.com>
2020-03-04 15:16:41 +0100

    Supprimer une annonce

--- src/Controller/AdController.php
+++ src/Controller/AdController.php
@@ -141,5 +141,27 @@ class AdController extends AbstractController
         ]);
     }
 
+    /**
+     * Permet de supprimer une annonce
+     * 
+     * @Route("/ads/{slug}/delete", name="ads_delete")
+     * @Security("is_granted('ROLE_USER') and user == ad.getAuthor()", message="Vous n'avez pas accès à cette ressource")
+     *
+     * @param Ad $ad
+     * @param EntityManagerInterface $manager
+     * @return void
+     */
+    public function delete(Ad $ad, EntityManagerInterface $manager){
+        $manager->remove($ad);
+        $manager->flush();
+
+        $this->addFlash(
+            'success',
+            "L'annonce <strong>{$ad->getTitle()}</strong> a bien été supprimée !"
+        );
+
+        return $this->redirectToRoute("ads_index");
+    }
+
     
 }
