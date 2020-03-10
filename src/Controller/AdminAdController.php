a127bee7fce153580488d4bc6e19b2a9b29022a4
Dolingale <contactdwwm@gmail.com>
2020-03-10 09:15:16 +0100

    Edition ad + front

--- src/Controller/AdminAdController.php
+++ src/Controller/AdminAdController.php
@@ -2,18 +2,52 @@
 
 namespace App\Controller;
 
+use App\Entity\Ad;
+use App\Form\AnnonceType;
 use App\Repository\AdRepository;
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 
 class AdminAdController extends AbstractController
 {
     /**
-     * @Route("/admin/ad", name="admin_ads_index")
+     * @Route("/admin/ads", name="admin_ads_index")
+     *
      */
     public function index(AdRepository $repo){
         return $this->render('admin/ad/index.html.twig', [
             'ads' => $repo->findAll(),
+        ]);
+    }
+
+    /**
+     * Permet d'afficher le formulaire d'édition
+     * 
+     * @Route("/admin/ad/{id}/edit", name="admin_ads_edit")
+     *
+     * @param Ad $ad
+     * @return Response
+     */
+    public function edit(Ad $ad, Request $req, EntityManagerInterface $manager){
+        $form = $this->createForm(AnnonceType::class, $ad);
+
+        $form->handleRequest($req);
+
+        if ($form->isSubmitted() && $form->isValid()){
+            $manager->persist($ad);
+            $manager->flush();
+
+            $this->addFlash(
+                'success',
+                "L'annonce <strong>{$ad->getTitle()}</strong> à bien été enregistrée !"
+            );
+        }
+
+        return $this->render('admin/ad/edit.html.twig',[
+            'ad' => $ad,
+            'form' => $form->createView()
         ]);
     }
 }
